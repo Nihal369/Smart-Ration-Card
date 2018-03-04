5857e519251c8df4841bf0d404a27f6f521738d4
nihal369 <nihalismailk@gmail.com>
2018-03-04 20:13:32 +0530

    Admin panel (Local) finished

--- app/src/main/java/com/smartcard/smartrationcard/AdminPanel.java
+++ app/src/main/java/com/smartcard/smartrationcard/AdminPanel.java
@@ -1,14 +1,21 @@
 package com.smartcard.smartrationcard;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import es.dmoral.toasty.Toasty;
 
 public class AdminPanel extends AppCompatActivity {
 
     //Object Decleration
     EditText ricePriceEditText,wheatPriceEditText,pulsesPriceEditText,kerosenePriceEditText,riceQuantityEditText,wheatQuantityEditText,
     keroseneQuantityEditText,pulsesQuantityEditText,aplFactorEditText,bplFactorEditText;
+
+    String ricePrice,wheatPrice,pulsesPrice,kerosenePrice,riceQuantity,wheatQuantity,keroseneQuantity,pulsesQuantity,aplFactor,bplFactor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,15 +36,134 @@ public class AdminPanel extends AppCompatActivity {
 
 
         //Set hint of the edit texts
-        ricePriceEditText.setHint(RationShop.getRicePrice());
-        wheatPriceEditText.setHint(RationShop.getWheatPrice());
-        kerosenePriceEditText.setHint(RationShop.getKerosenePrice());
-        pulsesPriceEditText.setHint(RationShop.getPulsesPrice());
-        riceQuantityEditText.setHint(RationShop.getRiceQuantity());
-        wheatQuantityEditText.setHint(RationShop.getWheatQuanity());
-        keroseneQuantityEditText.setHint(RationShop.getKeroseneQuantity());
-        pulsesQuantityEditText.setHint(RationShop.getPulsesQuantity());
-        aplFactorEditText.setHint(RationShop.getAplFactor());
-        bplFactorEditText.setHint(RationShop.getBplFactor());
+        ricePriceEditText.setText(String.valueOf(RationShop.getRicePrice()));
+        wheatPriceEditText.setText(String.valueOf(RationShop.getWheatPrice()));
+        kerosenePriceEditText.setText(String.valueOf(RationShop.getKerosenePrice()));
+        pulsesPriceEditText.setText(String.valueOf(RationShop.getPulsesPrice()));
+        riceQuantityEditText.setText(String.valueOf(RationShop.getRiceQuantity()));
+        wheatQuantityEditText.setText(String.valueOf(RationShop.getWheatQuanity()));
+        keroseneQuantityEditText.setText(String.valueOf(RationShop.getKeroseneQuantity()));
+        pulsesQuantityEditText.setText(String.valueOf(RationShop.getPulsesQuantity()));
+        aplFactorEditText.setText(String.valueOf(RationShop.getAplFactor()));
+        bplFactorEditText.setText(String.valueOf(RationShop.getBplFactor()));
+    }
+
+
+    //Update Prices in the RationShop Class
+    public void updatePrices(View view) {
+        try {
+            ricePrice = ricePriceEditText.getText().toString();
+            RationShop.setRicePrice(Integer.parseInt(ricePrice));
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
+        try {
+            wheatPrice = wheatPriceEditText.getText().toString();
+            RationShop.setWheatPrice(Integer.parseInt(wheatPrice));
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
+
+        try {
+            pulsesPrice = pulsesPriceEditText.getText().toString();
+            RationShop.setPulsesPrice(Integer.parseInt(pulsesPrice));
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
+
+        try {
+            kerosenePrice = kerosenePriceEditText.getText().toString();
+            RationShop.setKerosenePrice(Integer.parseInt(kerosenePrice));
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
+
+        try {
+            riceQuantity = riceQuantityEditText.getText().toString();
+            RationShop.setRiceQuantity(Integer.parseInt(riceQuantity));
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
+
+        try {
+            wheatQuantity = wheatQuantityEditText.getText().toString();
+            RationShop.setWheatQuantity(Integer.parseInt(wheatQuantity));
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
+
+        try {
+            pulsesQuantity = pulsesQuantityEditText.getText().toString();
+            RationShop.setPulsesQuantity(Integer.parseInt(pulsesQuantity));
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
+
+        try {
+            keroseneQuantity = keroseneQuantityEditText.getText().toString();
+            RationShop.setKeroseneQuantity(Integer.parseInt(keroseneQuantity));
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
+
+        try {
+            aplFactor = aplFactorEditText.getText().toString();
+            RationShop.setAplFactor(Integer.parseInt(aplFactor));
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
+
+        try {
+            bplFactor = bplFactorEditText.getText().toString();
+            RationShop.setBplFactor(Integer.parseInt(bplFactor));
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
+
+
+        //Display a success toast
+        Toasty.success(this, "Update Successful", Toast.LENGTH_SHORT).show();
+
+        //Move to RFID_READ Activity
+        Intent intent = new Intent(this, RFID_Read.class);
+        startActivity(intent);
+        finish();
+    }
+
+    public void cancelActivity(View view)
+    {
+        Intent intent=new Intent(this,RFID_Read.class);
+        startActivity(intent);
+        finish();
     }
 }

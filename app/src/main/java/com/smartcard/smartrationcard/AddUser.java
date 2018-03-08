d15b798c8ead6c834824d82f2c2aa3333cf6656b
nihal369 <nihalismailk@gmail.com>
2018-03-08 21:20:26 +0530

    Adding users added

--- app/src/main/java/com/smartcard/smartrationcard/AddUser.java
+++ app/src/main/java/com/smartcard/smartrationcard/AddUser.java
@@ -2,12 +2,69 @@ package com.smartcard.smartrationcard;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class AddUser extends AppCompatActivity {
+
+    //Object Decelerations
+    EditText rationCardEditText,nameEditText,numberOfFamilyMembersEditText,mobileNumberEditText,emailEditText;
+    //RadioGroup is a container that holds Radio Buttons
+    RadioGroup categoryRadioGroup;
+    String rationCardNumber,userName,numberOfFamilyMembers,mobileNumber,emailID,userCategory;
+    DatabaseReference mRootRef,numberRef,userRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_user);
+
+        //Object initializations
+        rationCardEditText=findViewById(R.id.numberEditText);
+        nameEditText=findViewById(R.id.nameEditText);
+        numberOfFamilyMembersEditText=findViewById(R.id.familyMembersEditText);
+        mobileNumberEditText=findViewById(R.id.phoneNumberEditText);
+        emailEditText=findViewById(R.id.emailEditText);
+        categoryRadioGroup=findViewById(R.id.categoryRadioGroup);
+    }
+
+    public void addUser(View view)
+    {
+        //Get texts from the text fields
+        rationCardNumber=rationCardEditText.getText().toString();
+        userName=nameEditText.getText().toString();
+        numberOfFamilyMembers=numberOfFamilyMembersEditText.getText().toString();
+        mobileNumber=mobileNumberEditText.getText().toString();
+        emailID=emailEditText.getText().toString();
+
+        //Get the selected button inside the radio group
+        if(categoryRadioGroup.getCheckedRadioButtonId()==R.id.bplButton)
+        {
+            userCategory="BPL";
+        }
+        else
+        {
+            userCategory="APL";
+        }
+
+
+        //Get the database reference
+        mRootRef = FirebaseDatabase.getInstance().getReference();
+        numberRef = mRootRef.child("rationcardnumber");
+        userRef=numberRef.child(rationCardNumber);
+
+        //Add details to the created child
+        userRef.child("category").setValue(userCategory);
+        userRef.child("familyMembers").setValue(numberOfFamilyMembers);
+        userRef.child("name").setValue(userName);
+        userRef.child("numberOfRations").setValue("0");
+        userRef.child("profPic").setValue("https://cdn2.iconfinder.com/data/icons/ios-7-icons/50/user_male2-256.png");//Standard Image
+        userRef.child("email").setValue(emailID);
+        userRef.child("phoneNumber").setValue(mobileNumber);
+
     }
 }
--- app/src/main/java/com/smartcard/smartrationcard/RemoveUser.java
+++ app/src/main/java/com/smartcard/smartrationcard/RemoveUser.java
@@ -1,13 +1,102 @@
 package com.smartcard.smartrationcard;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.shashank.sony.fancydialoglib.Animation;
+import com.shashank.sony.fancydialoglib.FancyAlertDialog;
+import com.shashank.sony.fancydialoglib.FancyAlertDialogListener;
+import com.shashank.sony.fancydialoglib.Icon;
+
+import es.dmoral.toasty.Toasty;
 
 public class RemoveUser extends AppCompatActivity {
+
+    //Object decelerations
+    EditText removeUserEditText;
+    String rationCardNumber;
+    DatabaseReference mRootRef,numberRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_remove_user);
+
+        //Object initialization
+        removeUserEditText=findViewById(R.id.removeUserEditText);
     }
+
+    public void removeUser(View view)
+    {
+        //Get the text entered in the editText form
+        rationCardNumber=removeUserEditText.getText().toString();
+
+        //Get the firebase reference
+        mRootRef = FirebaseDatabase.getInstance().getReference();
+        numberRef = mRootRef.child("rationcardnumber");
+
+        //Check if user exists
+        numberRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                //Check if the user for the ration card number entered exists
+                if(dataSnapshot.exists())
+                {
+                    //Delete the entire node corresponding
+                    new FancyAlertDialog.Builder(RemoveUser.this)
+                            .setTitle("Attend the victim")
+                            .setBackgroundColor(Color.parseColor("#F44336"))  //Don't pass R.color.colorvalue
+                            .setMessage("Are you sure you want to delete the user")
+                            .setNegativeBtnText("No")
+                            .setPositiveBtnBackground(Color.parseColor("#D32F2F"))  //Don't pass R.color.colorvalue
+                            .setPositiveBtnText("Yes")
+                            .setNegativeBtnBackground(Color.parseColor("#FFA9A7A8"))  //Don't pass R.color.colorvalue
+                            .setAnimation(Animation.POP)
+                            .isCancellable(true)
+                            .setIcon(R.drawable.ic_error_outline_white_48dp, Icon.Visible)
+
+                            .OnPositiveClicked(new FancyAlertDialogListener() {
+                                @Override
+                                public void OnClick() {
+
+                                    numberRef.child(rationCardNumber).removeValue();
+                                    Toasty.success(RemoveUser.this, rationCardNumber + "deleted", Toast.LENGTH_SHORT).show();
+
+                                }
+                            })
+
+                            .OnNegativeClicked(new FancyAlertDialogListener() {
+                                @Override
+                                public void OnClick() {
+                                }
+                            })
+                            .build();
+
+
+                }
+                else
+                {
+                    Toasty.error(RemoveUser.this, "User does not exist", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+
+    }
+
 }

aa532f99c795de265af9ad571298b55f2fabd55c
nihal369 <nihalismailk@gmail.com>
2018-03-14 19:27:35 +0530

    SMS and EMAIL added

--- app/src/main/java/com/smartcard/smartrationcard/RationDistribution.java
+++ app/src/main/java/com/smartcard/smartrationcard/RationDistribution.java
@@ -7,7 +7,18 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.creativityapps.gmailbackgroundlibrary.BackgroundMail;
 import com.squareup.picasso.Picasso;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class RationDistribution extends AppCompatActivity {
 
@@ -69,7 +80,7 @@ public class RationDistribution extends AppCompatActivity {
         riceText.setText("Rice:"+(familyMembers*RationShop.getRiceQuantity()*aplOrBplFactor)+"KG");
         wheatText.setText("Wheat:"+(familyMembers*RationShop.getWheatQuanity()*aplOrBplFactor)+"KG");
         keroseneText.setText("Kerosene:"+(familyMembers*RationShop.getKeroseneQuantity()*aplOrBplFactor)+"LTR");
-        pulsesText.setText("Rice:"+(familyMembers*RationShop.getPulsesQuantity()*aplOrBplFactor)+"KG");
+        pulsesText.setText("Pulses:"+(familyMembers*RationShop.getPulsesQuantity()*aplOrBplFactor)+"KG");
 
         ricePriceText.setText("PRICE:"+(RationShop.getRicePrice()/aplOrBplFactor)+" RS/KG");
         wheatPriceText.setText("PRICE:"+(RationShop.getWheatPrice()/aplOrBplFactor)+" RS/KG");
@@ -88,8 +99,129 @@ public class RationDistribution extends AppCompatActivity {
 
     public void completePurchase(View view)
     {
+        sendEmail();
+        sendSMS();
+        //Move to next activity
         Intent intent=new Intent(RationDistribution.this,PurchaseComplete.class);
         startActivity(intent);
         finish();
     }
+
+
+    private void sendEmail()
+    {
+        //GET THE CURRENT DATE
+        Date date = Calendar.getInstance().getTime();
+        SimpleDateFormat df = new SimpleDateFormat("dd-MMM-yyyy");
+        String formattedDate = df.format(date);
+
+
+        //EDIT CONTENT OF EMAIL HERE
+        String emailContent=
+                        "Details of your purchase from Ration Shop on "+formattedDate
+                        +"\n"
+                        +"CUSTOMER:"
+                        +LocalDB.getUserName()
+                        +"\n"
+                        +"RATION CARD NUMBER:"
+                        +LocalDB.getRationCardID()
+                        +"\n\n\n"
+
+                        +"Rice:"+(familyMembers*RationShop.getRiceQuantity()*aplOrBplFactor)+"KG:"
+                        +familyMembers*RationShop.getRiceQuantity()*RationShop.getRicePrice()*aplOrBplFactor
+                        +"\n"
+                        +"Wheat:"+(familyMembers*RationShop.getWheatQuanity()*aplOrBplFactor)+"KG:"
+                        +familyMembers*RationShop.getWheatQuanity()*RationShop.getWheatPrice()*aplOrBplFactor
+                        +"\n"
+                        +"Kerosene:"+(familyMembers*RationShop.getKeroseneQuantity()*aplOrBplFactor)+"LTR:"
+                        +familyMembers*RationShop.getKeroseneQuantity()*RationShop.getKerosenePrice()*aplOrBplFactor
+                        +"\n"
+                        +"Pulses:"+(familyMembers*RationShop.getPulsesQuantity()*aplOrBplFactor)+"KG:"
+                        +familyMembers*RationShop.getPulsesQuantity()*RationShop.getPulsesPrice()*aplOrBplFactor
+                        +"\n"
+                        +"TOTAL PRICE:"
+                        +totalPrice;
+
+
+        BackgroundMail.newBuilder(this)
+                .withUsername("smartrationcardvjc@gmail.com")
+                .withPassword("agnirose")
+                //TODO:REPLACE WITH CUSTOMER MAIL ID
+                .withMailto("nihalismailk@gmail.com")
+                .withType(BackgroundMail.TYPE_PLAIN)
+                .withSubject("Invoice for Ration Shop Purchase")
+                .withBody(emailContent)
+                .withOnSuccessCallback(new BackgroundMail.OnSuccessCallback() {
+                    @Override
+                    public void onSuccess() {
+
+                    }
+                })
+                .withOnFailCallback(new BackgroundMail.OnFailCallback() {
+                    @Override
+                    public void onFail() {
+
+                    }
+                })
+                .send();
+    }
+
+
+    private void sendSMS()
+    {
+
+        //EDIT CONTENT OF SMS HERE
+        String smsContent=
+                        "CUSTOMER:"
+                        +LocalDB.getUserName()
+                        +"\n\n\n"
+
+                        +"Rice:"+(familyMembers*RationShop.getRiceQuantity()*aplOrBplFactor)+"KG:"
+                        +familyMembers*RationShop.getRiceQuantity()*RationShop.getRicePrice()*aplOrBplFactor
+                        +"\n"
+                        +"Wheat:"+(familyMembers*RationShop.getWheatQuanity()*aplOrBplFactor)+"KG:"
+                        +familyMembers*RationShop.getWheatQuanity()*RationShop.getWheatPrice()*aplOrBplFactor
+                        +"\n"
+                        +"Kerosene:"+(familyMembers*RationShop.getKeroseneQuantity()*aplOrBplFactor)+"LTR:"
+                        +familyMembers*RationShop.getKeroseneQuantity()*RationShop.getKerosenePrice()*aplOrBplFactor
+                        +"\n"
+                        +"Pulses:"+(familyMembers*RationShop.getPulsesQuantity()*aplOrBplFactor)+"KG:"
+                        +familyMembers*RationShop.getPulsesQuantity()*RationShop.getPulsesPrice()*aplOrBplFactor
+                        +"\n"
+                        +"TOTAL PRICE:"
+                        +totalPrice;
+        try {
+            // Construct data
+            String data = "";
+            data += "username=" + URLEncoder.encode("nihal369", "ISO-8859-1");
+            data += "&password=" + URLEncoder.encode("agnirose", "ISO-8859-1");
+            data += "&message=" + URLEncoder.encode(smsContent, "ISO-8859-1");
+            data += "&want_report=1";
+            //TODO:REPLACE WITH CUSTOMER NUMBER
+            data += "&msisdn=07356647169";// relace with the number
+
+            // Send data
+            URL url = new URL("http://bulksms.vsms.net:5567/eapi/submission/send_sms/2/2.0");
+
+            URLConnection conn = url.openConnection();
+            conn.setDoOutput(true);
+            OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
+            wr.write(data);
+            wr.flush();
+
+            // Get the response
+            BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            String line;
+            while ((line = rd.readLine()) != null) {
+                // Print the response output...
+                System.out.println(line);
+            }
+            wr.close();
+            rd.close();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 }

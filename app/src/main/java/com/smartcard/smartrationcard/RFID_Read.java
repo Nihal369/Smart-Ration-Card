274cee6f93d3ab7903989600e2e6049250ee5d56
nihal369 <nihalismailk@gmail.com>
2018-04-11 23:29:34 +0530

    NFC Detection done

--- app/src/main/java/com/smartcard/smartrationcard/RFID_Read.java
+++ app/src/main/java/com/smartcard/smartrationcard/RFID_Read.java
@@ -1,8 +1,14 @@
 package com.smartcard.smartrationcard;
 
 import android.annotation.SuppressLint;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.nfc.NdefMessage;
 import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -16,60 +22,32 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.io.UnsupportedEncodingException;
+
 import es.dmoral.toasty.Toasty;
 
 public class RFID_Read extends AppCompatActivity {
 
-    //Object and Variable Decelerations
-    private String rfidTagValue;
-    private NfcAdapter mNFCAdapter;
-    EditText editText;
-    private DatabaseReference mRootRef,rationCardNumberRef,userRef;
+    private NfcAdapter nfcAdapter;
 
-    @SuppressLint("CheckResult")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rfid__read);
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this);
 
-        //Initialize the edit text object
-        editText=findViewById(R.id.editText);
-    }
-
-    //Manually Enter Ration Card Number
-    public void enterRfidTagValue(View view)
-    {
-        if(editText.getText()!=null) {
-            rfidTagValue = editText.getText().toString();
-            mRootRef = FirebaseDatabase.getInstance().getReference();
-            //Get the child reference
-            rationCardNumberRef = mRootRef.child("rationcardnumber");
-            //rationcardnumber->1000,1001 etc
-            userRef = rationCardNumberRef.child(rfidTagValue);
-            userRef.addListenerForSingleValueEvent(new ValueEventListener() {
-                @Override
-                public void onDataChange(DataSnapshot dataSnapshot) {
-                    if(dataSnapshot.exists())
-                    {
-                        LocalDB.setRationCardID(rfidTagValue);
-                        Intent intent = new Intent(RFID_Read.this, FingerPrint.class);
-                        startActivity(intent);
-                    }
-
-                    else
-                    {
-                        Toasty.error(RFID_Read.this, "Invalid User", Toast.LENGTH_SHORT).show();
-                    }
-                }
-
-                @Override
-                public void onCancelled(DatabaseError databaseError) {
-
-                }
-            });
-
+        if(nfcAdapter == null)
+        {
+            Toasty.error(this,"PHONE DOES NOT HAVE NFC",Toast.LENGTH_LONG).show();
+        }
+        else if(!nfcAdapter.isEnabled())
+        {
+            Toasty.warning(this,"PLEASE ENABLE NFC",Toast.LENGTH_LONG).show();
         }
     }
+
+
+
 
     //Move to AdminLogin Activity
     public void moveToAdminLogin(View view)
